3e1de2732828ed48109f3814bd0998863a77d513
yueyueniao_hadink <let_s_go_hadink@126.com>
2021-07-28 23:30:46 +0800

    test1

---
+++ Hello/src/main/java/com/atguigu/maven/Hello.java
@@ -0,0 +1,7 @@
+package com.atguigu.maven;
+
+public class Hello {
+    public String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
---
+++ Hello/src/test/java/com/atguigu/maven/TestHello.java
@@ -0,0 +1,19 @@
+package com.atguigu.maven;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TestHello {
+    @Test
+    public void testHello() {
+        String name = "atguigu";
+        Hello hello = new Hello();
+        String resule = hello.sayHello(name);
+        assertEquals("Hello atguigu!", resule);
+        String s = "test测试撒地方看见了撒旦JFK";
+        System.out.println(s);
+
+
+    }
+}
